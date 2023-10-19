3068495e3a1a27944d4be2e1e3f90964b3298ef2
Rasmus Glasell Johansson <ragl025@gmail.com>
2023-10-19 10:10:02 +0200

    Add project files.

---
+++ Labb1_Grunderna_i_OOP/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+// RASMUS GLASELL JOHANSSON SUT23
+
+
+namespace Labb1_Grunderna_i_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle radius1 = new Circle(3.141f, 5);
+            Circle radius2 = new Circle(3.141f, 6);
+
+            radius1.getArea();
+            radius2.getArea();
+        }
+    }
+}
